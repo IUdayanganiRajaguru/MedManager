3d28a29f44d38c1df4250af4b1cae94b48e71505
Udayangani Rajaguru <62050460+IUdayanganiRajaguru@users.noreply.github.com>
2020-11-11 19:06:22 +0530

    init commit

---
+++ src/test/java/com/finalproject/med_manager/MedManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.med_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
